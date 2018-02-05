4d44b6a00f30c5c797d684bd91e95738c7c6444e
Cody Van De Mark <cavigm@rit.edu>
2018-02-05 16:37:00 -0500

    updated with comments

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -4,17 +4,22 @@
 // We will be working with databases in the next few weeks.
 const users = {};
 
+//function to respond with a json object
+//takes request, response, status code and object to send
 const respondJSON = (request, response, status, object) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.write(JSON.stringify(object));
   response.end();
 };
 
+//function to respond without json body
+//takes request, response and status code
 const respondJSONMeta = (request, response, status) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
   response.end();
 };
 
+//return user object as JSON
 const getUsers = (request, response) => {
   const responseJSON = {
     users,
@@ -23,10 +28,51 @@ const getUsers = (request, response) => {
   respondJSON(request, response, 200, responseJSON);
 };
 
+//function to add a user from a POST body
 const addUser = (request, response, body) => {
+  //default json message
+  const responseJSON = {
+    message: 'Name and age are both required.',
+  };
 
+  //check to make sure we have both fields
+  //We might want more validation than just checking if they exist
+  //This could easily be abused with invalid types (such as booleans, numbers, etc)
+  //If either are missing, send back an error message as a 400 badRequest
+  if (!body.name || !body.age) {
+    responseJSON.id = 'missingParams';
+    return respondJSON(request, response, 400, responseJSON);
+  }
+
+  //default status code to 201 created
+  let responseCode = 201;
+
+  //if that user's name already exists in our object
+  //then switch to a 204 updated status
+  if (users[body.name]) {
+    responseCode = 204;
+  } else {
+    //otherwise create an object with that name
+    users[body.name] = {};
+  }
+
+  //add or update fields for this user name
+  users[body.name].name = body.name;
+  users[body.name].age = body.age;
+
+  //if response is created, then set our created message
+  //and sent response with a message
+  if (responseCode === 201) {
+    responseJSON.message = 'Created Successfully';
+    return respondJSON(request, response, responseCode, responseJSON);
+  }
+  // 204 has an empty payload, just a success
+  // It cannot have a body, so we just send a 204 without a message
+  // 204 will not alter the browser in any way!!!
+  return respondJSONMeta(request, response, responseCode);
 };
 
+//public exports
 module.exports = {
   getUsers,
   addUser,
--- src/server.js
+++ src/server.js
@@ -1,16 +1,57 @@
-const http = require('http');
-const url = require('url');
+const http = require('http'); //pull in http module
+//url module for parsing url string
+const url = require('url'); 
+//querystring module for parsing querystrings from url
 const query = require('querystring');
+//pull in our custom files
 const htmlHandler = require('./htmlResponses.js');
 const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
+//handle POST requests
 const handlePost = (request, response, parsedUrl) => {
+  //if post is to /addUser (our only POST url)
+  if (parsedUrl.pathname === '/addUser') {
+    const res = response;
 
+    //uploads come in as a byte stream that we need 
+    //to reassemble once it's all arrived
+    const body = [];
+
+    //if the upload stream errors out, just throw a
+    //a bad request and send it back 
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    //on 'data' is for each byte of data that comes in
+    //from the upload. We will add it to our byte array.
+    request.on('data', (chunk) => {
+      body.push(chunk); 
+    });
+
+    //on end of upload stream. 
+    request.on('end', () => {
+      //combine our byte array (using Buffer.concat)
+      //and convert it to a string value (in this instance)
+      const bodyString = Buffer.concat(body).toString();
+      //since we are getting x-www-form-urlencoded data
+      //the format will be the same as querystrings
+      //Parse the string into an object by field name
+      const bodyParams = query.parse(bodyString);
+
+      //pass to our addUser function
+      jsonHandler.addUser(request, res, bodyParams);
+    });
+  }
 };
 
+//handle GET requests
 const handleGet = (request, response, parsedUrl) => {
+  //route to correct method based on url
   if (parsedUrl.pathname === '/style.css') {
     htmlHandler.getCSS(request, response);
   } else if (parsedUrl.pathname === '/getUsers') {
@@ -21,9 +62,17 @@ const handleGet = (request, response, parsedUrl) => {
 };
 
 const onRequest = (request, response) => {
+  //parse url into individual parts
+  //returns an object of url parts by name
   const parsedUrl = url.parse(request.url);
 
-
+  //check if method was POST, otherwise assume GET 
+  //for the sake of this example
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
 };
 
 http.createServer(onRequest).listen(port);
