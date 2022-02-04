fce7b448161134ab90b0d4bafab9f217cec619e0
Victoria Olivieri <70556187+vickyrose456@users.noreply.github.com>
2022-02-04 18:58:36 -0500

    Video Tutorial

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -24,46 +24,41 @@ const getUsers = (request, response) => {
 };
 
 const addUser = (request, response, body) => {
-  //assume body is info sent by the usr
+  // assume body is info sent by the usr
 
-
-  //assume they didnt send the info you asked for
+  // assume they didnt send the info you asked for
   const responseJSON = {
-    messgae: 'Name and age are both required'
+    messgae: 'Name and age are both required',
   };
 
-  if(!body.name || !body.age)
-  {
+  if (!body.name || !body.age) {
     responseJSON.id = 'missingParams';
     return respondJSON(request, response, 400, responseJSON);
-  }else{}
-
-  //the user doesnt exist.. or they need to be updated
-  //not exist
-  let responseCode = 201;
-
-  //you would want to use a username irl
-  if(users[body.name]){
-    //user already exist 
-    responseCode = 204;
-  }else{
-    //there is no object 
-    users[body.name ] = {};
-
   }
 
-  users[body.name].name = body.name;
+  // the user doesnt exist.. or they need to be updated
+  // not exist
+  let responseCode = 201;
+
+  // you would want to use a username irl
+  if (users[body.name]) {
+    // user already exist
+    responseCode = 204;
+  } else {
+    // there is no object
+    users[body.name] = {};
+    users[body.name].name = body.name;
+  }
+
   users[body.name].age = body.age;
 
-  //send appropriate responses
-  if(responseCode === 201)
-  {
+  // send appropriate responses
+  if (responseCode === 201) {
     responseJSON.messgae = 'Created Successfully';
-    return respondJSON(request, resposnse, responseCode, responseJSON);
+    return respondJSON(request, response, responseCode, responseJSON);
   }
 
   return respondJSONMeta(request, response, responseCode);
-
 };
 
 module.exports = {
--- src/server.js
+++ src/server.js
@@ -7,7 +7,33 @@ const jsonHandler = require('./jsonResponses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const handlePost = (request, response, parsedUrl) => {
+  if (parsedUrl.pathname === '/addUser') {
+    const body = [];// buffer
 
+    // event based system
+    request.on('error', (err) => {
+      // callback fns coming from HTTP library
+      console.dir(err);
+      response.statusCode = 400;
+      response.end();
+    });
+
+    // data event
+    request.on('data', (chunk) => {
+      // want to add chunk of data to the array
+      body.push(chunk);
+    });
+
+    // end event
+    request.on('end', () => {
+      // built in fns for array.., takes all elements and combines them into a single element
+      const bodyString = Buffer.concat(body).toString();
+      const bodyParams = query.parse(bodyString);
+
+      // call our handler
+      jsonHandler.addUser(request, response, bodyParams);
+    });
+  }
 };
 
 const handleGet = (request, response, parsedUrl) => {
@@ -23,7 +49,11 @@ const handleGet = (request, response, parsedUrl) => {
 const onRequest = (request, response) => {
   const parsedUrl = url.parse(request.url);
 
-
+  if (request.method === 'POST') {
+    handlePost(request, response, parsedUrl);
+  } else {
+    handleGet(request, response, parsedUrl);
+  }
 };
 
 http.createServer(onRequest).listen(port, () => {
